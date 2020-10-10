6f829d451b05d0d9cac602aedb458d19fdf76434
William <gw826943555@qq.com>
2020-10-10 14:00:08 +0800

    revert custom feeds

--- custom.sh
+++ custom.sh
@@ -7,6 +7,5 @@ sed -i "/timezone/a\\\t\tset system.@system[-1].zonename='Asia/Shanghai'" ./pack
 sed -i '/memberships/a\\\t\tnet.ipv4.tcp_fastopen=3' ./package/base-files/files/etc/sysctl.d/10-default.conf
 sed -i 's/nf_conntrack_max=16384/nf_conntrack_max=65536/g' ./package/kernel/linux/files/sysctl-nf-conntrack.conf
 sed -i 's/radio${devidx}.disabled=1/radio${devidx}.disabled=0/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/lede#g' ./package/base-files/image-config.in
 
 echo 'src-git misc https://github.com/gw826943555/openwrt-packages-misc' >> ./feeds.conf.default
